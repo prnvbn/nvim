55d6b663aae146d711e22da64468e3e273f74ec5
Pranav Bansal <prnvbn@gmail.com>
2024-07-09 16:27:02 +0100

    rm enure_installed from treesitter cfg
    this can be done as aut_install is set

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,26 +8,6 @@ return {
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
-			ensure_installed = {
-				"lua",
-				-- Golang
-				"go",
-				"gomod",
-				"gosum",
-				"gowork",
-				-- Rust
-				"rust",
-				-- Python
-				"python",
-				"c",
-				"vim",
-				"vimdoc",
-				"query",
-				"elixir",
-				"heex",
-				"javascript",
-				"html",
-			},
 		})
 	end,
 }
